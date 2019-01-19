5ae925f66d071ad366263b736e53040fff1508ba
Ekaterina <ekaterina.kalinina.1998@gmail.com>
2019-01-20 00:13:35 +0300

    Add addHiveSection, updateHiveSection and deleteHiveSection methods implementation to HiveSectionService. Add fields to HiveSection model. Add HiveSectionFormComponent layout. Add HiveSectionFormComponent methods implementation. Add routes to AppRouting module.

--- KatlaSportNg/src/app/hive-management/forms/hive-section-form.component.ts
+++ KatlaSportNg/src/app/hive-management/forms/hive-section-form.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HiveSectionService } from '../services/hive-section.service';
+import { HiveSection } from '../models/hive-section';
+import { HiveListItem } from '../models/hive-list-item';
+import { HiveService } from '../services/hive.service';
 
 @Component({
   selector: 'app-hive-section-form',
@@ -7,8 +12,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HiveSectionFormComponent implements OnInit {
 
-  constructor() { }
+  hiveSection = new HiveSection(0, "", "", 0, false, "" );
+  existed = false;
+  hiveId: number;
+  hives: HiveListItem[];
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private hiveSectionService: HiveSectionService,
+    private hiveService: HiveService
+  ) { }
 
   ngOnInit() {
+    this.hiveService.getHives().subscribe(h => this.hives = h);
+    this.route.params.subscribe(p => {
+      this.hiveId = p['hiveId'];
+      if (p['id'] === undefined) return;
+      this.hiveSectionService.getHiveSection(p['id']).subscribe(s => this.hiveSection = s);
+      this.existed = true;
+    });
+  }
+
+  navigateToHiveSections() {
+    if (this.hiveId === undefined) {
+      this.router.navigate(['/hives']);
+    } else {
+      this.router.navigate([`/hive/${this.hiveId}/sections`]);
+    }
+  }
+
+  onCancel() {
+    this.navigateToHiveSections();
+  }
+
+  onSubmit() {
+    if (this.existed) {
+      this.hiveSectionService.updateHiveSection(this.hiveSection).subscribe(s => this.navigateToHiveSections());
+    } else {
+      this.hiveSection.storeHiveId = this.hiveId;
+      this.hiveSectionService.addHiveSection(this.hiveSection).subscribe(s => this.navigateToHiveSections());
+    }
+  }
+
+  onDelete() {
+    this.hiveSectionService.setHiveSectionStatus(this.hiveSection.id, true).subscribe(c => this.hiveSection.isDeleted = true);
+  }
+
+  onUndelete() {
+    this.hiveSectionService.setHiveSectionStatus(this.hiveSection.id, false).subscribe(c => this.hiveSection.isDeleted = false);
+  }
+
+  onPurge() {
+    this.hiveSectionService.deleteHiveSection(this.hiveSection.id).subscribe(c => this.navigateToHiveSections());
   }
 }
--- KatlaSportNg/src/app/hive-management/lists/hive-section-list.component.ts
+++ KatlaSportNg/src/app/hive-management/lists/hive-section-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HiveSectionListItem } from '../models/hive-section-list-item';
 import { HiveService } from '../services/hive.service';
+import { HiveSectionService } from '../services/hive-section.service';
 
 @Component({
   selector: 'app-hive-section-list',
@@ -16,7 +17,8 @@ export class HiveSectionListComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private hiveService: HiveService
+    private hiveService: HiveService,
+    private hiveSectionService: HiveSectionService
   ) { }
 
   ngOnInit() {
@@ -27,12 +29,12 @@ export class HiveSectionListComponent implements OnInit {
   }
 
   onDelete(hiveSectionId: number) {
-    var hive = this.hiveSections.find(h => h.id == hiveSectionId);
-    this.hiveService.setHiveStatus(hiveSectionId, true).subscribe(c => hive.isDeleted = true);
+    var hiveSection = this.hiveSections.find(s => s.id == hiveSectionId);
+    this.hiveSectionService.setHiveSectionStatus(hiveSectionId, true).subscribe(c => hiveSection.isDeleted = true);
   }
 
   onUndelete(hiveSectionId: number) {
-    var hive = this.hiveSections.find(h => h.id == hiveSectionId);
-    this.hiveService.setHiveStatus(hiveSectionId, false).subscribe(c => hive.isDeleted = false);
+    var hiveSection = this.hiveSections.find(s => s.id == hiveSectionId);
+    this.hiveSectionService.setHiveSectionStatus(hiveSectionId, false).subscribe(c => hiveSection.isDeleted = false);
   }
 }
