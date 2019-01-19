bd43ab4c07d78b1843c54f3ad49f6c35f5886a43
Ekaterina <ekaterina.kalinina.1998@gmail.com>
2019-01-19 22:13:18 +0300

    Add UpdateHiveSectionRequest and UpdateHiveSectionRequestValidator models to HiveManagement. Update HiveManagementMappingProfile. Add AddHiveSectionAsync, UpdateHiveSectionAsync and DeleteHiveSectionAsync methods implementation to HiveSectionService. Add AddHiveSection, UpdateHiveSection and DeleteHiveSection methods implementation to HiveSectionsController.

--- KatlaSport.Services.Models/HiveManagement/UpdateHiveSectionRequest.cs
+++ KatlaSport.Services.Models/HiveManagement/UpdateHiveSectionRequest.cs
@@ -1,23 +1,23 @@
 ﻿namespace KatlaSport.Services.HiveManagement
 {
     /// <summary>
-    /// Represents a request for creating and updating a hive.
+    /// Represents a request for creating and updating a hive section.
     /// </summary>
-    public class UpdateHiveRequest
+    public class UpdateHiveSectionRequest
     {
         /// <summary>
-        /// Gets or sets a hive name.
+        /// Gets or sets a hive section name.
         /// </summary>
         public string Name { get; set; }
 
         /// <summary>
-        /// Gets or sets a hive code.
+        /// Gets or sets a hive section code.
         /// </summary>
         public string Code { get; set; }
 
         /// <summary>
-        /// Gets or sets a hive address.
+        /// Gets or sets a hive ID.
         /// </summary>
-        public string Address { get; set; }
+        public int StoreHiveId { get; set; }
     }
 }
